c5e7c628fd8c2c583f74fbce7d88b22c27d153c7
Milagros Quintana <milagrosquintana735@gmail.com>
2026-05-25 17:46:50 -0300

    agrego el test para la clase User

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,24 @@
 import test from "ava";
-import { Product } from "./index";
+import { Product, User } from "./index";
 
 test("Testeo la clase Product", (t) => {
-  // test Product class
   const product = new Product("Product 1", 10);
+
   t.is(product.name, "Product 1");
   t.is(product.price, 10);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+test("Testeo la clase User y sus métodos", (t) => {
+  const user = new User("Mili");
+
+  //test del constructor
+  t.is(user.name, "Mili");
+  t.deepEqual(user.products, []);
+
+  //test del metodo addProduct
+  const product =new Product("Product 1", 10);
+
+  user.addProduct(product);
+
+  t.is(user.products.length, 1);
+  t.deepEqual(user.products[0], product);
+});
